7e8de962bf7ff4088ce967c6d92bf94d9c13ed44
Mehrdad <amini_pk@hotmail.com>
2026-01-15 19:36:11 +0100

    add avslutaInsats to LarmEnhet and implement in all units

--- src/AmbulansEnhet.java
+++ src/AmbulansEnhet.java
@@ -4,4 +4,9 @@ public class AmbulansEnhet implements LarmEnhet {
     public void hanteraLarm(String scenario) {
         System.out.println("🚑 Ambulans skickas till platsen.");
     }
+
+    @Override
+    public void avslutaInsats() {
+        System.out.println("🚑 Patienten är omhändertagen och ambulansen kör till sjukhus.");
+    }
 }
--- src/PolisEnhet.java
+++ src/PolisEnhet.java
@@ -4,4 +4,9 @@ public class PolisEnhet implements LarmEnhet {
     public void hanteraLarm(String scenario) {
         System.out.println("🚓 Polis skickas till platsen.");
     }
+
+    @Override
+    public void avslutaInsats() {
+        System.out.println("🚓 Polisen har gripit de kriminella och återvänder till stationen.");
+    }
 }
